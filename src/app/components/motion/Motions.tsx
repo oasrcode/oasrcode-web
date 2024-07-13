f891ab0b251667137347ed00f4a04b51e4b5f390
oasrcode <oasrcode@gmail.com>
2024-07-13 21:42:37 +0000

    added motions for framer motions

---
+++ src/app/components/motion/Motions.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+export const MotionButtonHeader= motion.a;
+export const MotionSpanHeaderHover= motion.span;
+export const MotionDivExperienceCardHover= motion.div;
+export const MotionSection= motion.section;
