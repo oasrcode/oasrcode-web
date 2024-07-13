3d92bfc29bc50cefc2021c48aec878832e53a5b5
oasrcode <oasrcode@gmail.com>
2024-07-13 21:42:24 +0000

    refactoring and improved header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,26 +1,34 @@
 "use client";
-import Image from "next/image";
-import { MotionButtonHeaderHover, MotionSpanHeaderHover } from "../motion/Motions";
+
+import { MotionButtonHeader, MotionSpanHeaderHover } from "./motion/Motions";
 import { useState } from "react";
 
 export default function Header() {
-  let tabs = ["Inicio", "Experiencia", "Proyectos", "Blog"];
+  const tabs = [
+    { label: "Inicio", href: "#home" },
+    { label: "Experiencia", href: "#experience" },
+    { label: "Proyectos", href: "#proyects" },
+    { label: "Blog", href: "/blog" }, // Ajuste aquí para ser consistente con otros hrefs
+  ];
+
   const [hovered, setHovered] = useState("Inicio");
+
   return (
-    <header className="sticky flex flex-row items-center justify-between w-full h-24 bg-emerald-900 ">
-      <Image className="ml-10" src="./next.svg" alt="logo" width="200" height="200" />
-      <ul className="flex flex-row items-center gap-20 mr-10">
-        {tabs.map((tab, index) => (
-          <MotionButtonHeaderHover 
-          onHoverStart={()=>setHovered(tab)}
-          
-          key={index} className="group relative hover:text-red-600  duration-200 px-3 py-1">
-            {tab}{hovered===tab&&(    <MotionSpanHeaderHover
-            layoutId="nav-item" className="absolute bg-gray-100 bg-opacity-10 inset-0 rounded-md p-2"></MotionSpanHeaderHover>)}
-        
-          </MotionButtonHeaderHover>
-        ))}
-      </ul>
+    <header className="sticky z-50 flex items-center top-0 w-full h-24 bg-slate-950">
+      <div className="flex flex-row items-center justify-between mx-auto w-full lg:w-3/5 backdrop:blur-lg text-white">
+        <p className="text-white text-3xl">
+          OASRCode<span className="text-4xl text-blue-500">.</span>
+        </p>
+        <ul className="flex flex-row items-center gap-20 mr-10">
+          {tabs.map((tab, index) => (
+            <MotionButtonHeader onHoverStart={() => setHovered(tab.label)} href={tab.href} key={index} className="group relative cursor-pointer hover:text-yellow-500 duration-200 px-4 py-3">
+              {tab.label}
+
+              {hovered === tab.label && <MotionSpanHeaderHover layoutId="nav-item" className="absolute bg-yellow-500 bg-opacity-30 inset-0 rounded-md p-2"></MotionSpanHeaderHover>}
+            </MotionButtonHeader>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 }
