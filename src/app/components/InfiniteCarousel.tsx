7567699a438c887e8adbb4e9a5e30197c912e979
oasrcode <oasrcode@gmail.com>
2024-08-13 21:02:02 +0000

    changes in infinite carousel

--- src/app/components/InfiniteCarousel.tsx
+++ src/app/components/InfiniteCarousel.tsx
@@ -4,17 +4,17 @@ import { MotionCarousel } from "./motion/Motions";
 export default function InfiniteCarousel({ icons, direction }: { icons: any[]; direction: any }) {
   const animateX = direction === "left" ? ["0%", "-100%"] : ["-100%", "0%"];
   return (
-    <div className="relative overflow-hidden whitespace-nowrap max-w-4xl mx-auto py-4">
+    <div className="relative overflow-hidden whitespace-nowrap lg:max-w-4xl mx-auto lg:py-4">
       <MotionCarousel className="inline-block whitespace-nowrap" animate={{ x: animateX }} transition={{ duration: 30, repeat: Infinity, ease: "linear" }}>
         {icons.concat(icons).map((icon, index) => (
-          <span key={index} className="text-4xl mx-16 inline-block">
+          <span key={index} className="lg:text-4xl lg:mx-16 inline-block mx-4">
             {icon}
           </span>
         ))}
       </MotionCarousel>
       <MotionCarousel className="inline-block whitespace-nowrap" animate={{ x: animateX }} transition={{ duration: 30, repeat: Infinity, ease: "linear" }}>
         {icons.concat(icons).map((icon, index) => (
-          <span key={index} className="mx-16 inline-block">
+          <span key={index} className="lg:mx-16 inline-block mx-4">
             {icon}
           </span>
         ))}
