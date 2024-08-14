1eac5537d4b2fc33906191b4a125403899abfa7a
oasrcode <oasrcode@gmail.com>
2024-08-14 22:11:42 +0000

    hide skill section

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -25,7 +25,7 @@ import TailwindIcon from "./icons/TailwindIcon";
 import TeamsIcon from "./icons/TeamsIcon";
 
 import TypeScriptIcon from "./icons/TypeScriptIcon";
-import InfiniteCarousel from "./InfiniteCarousel";
+import { MotionSkillIcon, MotionSkills } from "./motion/Motions";
 
 const frontendIcons = [
   AngularIcon({ height: 80, width: 80 }),
@@ -60,64 +60,57 @@ const ToolsIcons = [
 ];
 export default function Skills() {
   return (
-    // <div className="flex flex-col mt-20 gap-14">
-    //   <p className="font-mono text-2xl"><span className="text-pink-600 font-semibold">private </span><span className="text-blue-700 font-semibold">const </span>Frontend </p>
-    //   <InfiniteCarousel icons={frontendIcons} direction={"right"} />
-    //   <p className="font-mono text-2xl"><span className="text-pink-600 font-semibold">private </span><span className="text-blue-700 font-semibold">const </span>Backend</p>
-    //   <InfiniteCarousel icons={backendIcons} direction={"left"}/>
-    //   <p className="font-mono text-2xl"><span className="text-pink-600 font-semibold">private </span><span className="text-blue-700 font-semibold">const </span>Herramientas</p>
-    //   <InfiniteCarousel icons={ToolsIcons} direction={"right"}/>
+    <></>
+
+    // <div className="mt-20 bg-slate-950 rounded-lg">
+    //   <div className="relative flex items-center justify-between bg-gray-700 p-2 rounded-t-lg">
+    //     <span className="text-center text-sm text-gray-400 flex-grow ">Skills.ts - Visual Studio Code</span>
+    //     <div className="flex items-center gap-2">
+    //       <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+    //       <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+    //       <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+    //     </div>
+    //   </div>
+
+    //   <div className="flex flex-row gap-5">
+    //     <div className="flex flex-col text-white  text-opacity-80 p-3">
+    //       <span>23</span>
+    //       <span>24</span>
+    //       <span>25</span>
+    //       <span>26</span>
+    //       <span>27</span>
+    //       <span>28</span>
+    //       <span>29</span>
+    //       <span>30</span>
+    //       <span>31</span>
+    //       <span>32</span>
+    //       <span>33</span>
+    //       <span>34</span>
+    //       <span>35</span>
+    //       <span>36</span>
+    //       <span>37</span>
+    //       <span>38</span>
+    //       <span>39</span>
+    //       <span>40</span>
+    //       <span>41</span>
+    //       <span>42</span>
+    //     </div>
+
+    //     <div className="flex flex-col mt-2">
+    //       <p className="font-mono text-xl  items-center text-white">
+    //         <span className="text-pink-600 font-semibold">private </span><span className="text-blue-400 font-semibold"> const </span> Frontend
+    //       </p>
+    //       <InfiniteCarousel icons={frontendIcons} direction={"right"} />
+    //       <p className="font-mono text-xl  items-center text-white">
+    //         <span className="text-pink-600 font-semibold">private </span><span className="text-blue-400 font-semibold"> const </span> Backend
+    //       </p>
+    //       <InfiniteCarousel icons={backendIcons} direction={"left"} />
+    //       <p className="font-mono text-xl  items-center text-white">
+    //         <span className="text-pink-600 font-semibold">private </span><span className="text-blue-400 font-semibold"> const </span> Tools
+    //       </p>
+    //       <InfiniteCarousel icons={ToolsIcons} direction={"right"} />
+    //     </div>
+    //   </div>
     // </div>
-
-    <div className="mt-20 bg-slate-950 rounded-lg">
-      <div className="relative flex items-center justify-between bg-gray-700 p-2 rounded-t-lg">
-        <span className="text-center text-sm text-gray-400 flex-grow ">Skills.ts - Visual Studio Code</span>
-        <div className="flex items-center gap-2">
-          <div className="w-3 h-3 bg-red-500 rounded-full"></div>
-          <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
-          <div className="w-3 h-3 bg-green-500 rounded-full"></div>
-        </div>
-      </div>
-
-      <div className="flex flex-row gap-5">
-        <div className="flex flex-col text-white  text-opacity-80 p-3">
-          <span>23</span>
-          <span>24</span>
-          <span>25</span>
-          <span>26</span>
-          <span>27</span>
-          <span>28</span>
-          <span>29</span>
-          <span>30</span>
-          <span>31</span>
-          <span>32</span>
-          <span>33</span>
-          <span>34</span>
-          <span>35</span>
-          <span>36</span>
-          <span>37</span>
-          <span>38</span>
-          <span>39</span>
-          <span>40</span>
-          <span>41</span>
-          <span>42</span>
-        </div>
-
-        <div className="flex flex-col mt-2">
-          <p className="font-mono text-xl  items-center text-white">
-            <span className="text-pink-600 font-semibold">private </span><span className="text-blue-400 font-semibold"> const </span> Frontend
-          </p>
-          <InfiniteCarousel icons={frontendIcons} direction={"right"} />
-          <p className="font-mono text-xl  items-center text-white">
-            <span className="text-pink-600 font-semibold">private </span><span className="text-blue-400 font-semibold"> const </span> Backend
-          </p>
-          <InfiniteCarousel icons={backendIcons} direction={"left"} />
-          <p className="font-mono text-xl  items-center text-white">
-            <span className="text-pink-600 font-semibold">private </span><span className="text-blue-400 font-semibold"> const </span> Tools
-          </p>
-          <InfiniteCarousel icons={ToolsIcons} direction={"right"} />
-        </div>
-      </div>
-    </div>
   );
 }
