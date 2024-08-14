3cfb5d0085f50d32b2bed711d2694e5924ccad2e
oasrcode <oasrcode@gmail.com>
2024-08-14 22:12:37 +0000

    added section for projects and project cards

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,4 +1,44 @@
+import { Project } from "../interface/IProject";
+import DockerIcon from "./icons/DockerIcon";
+import GithubIcon from "./icons/Github";
+import GitIcon from "./icons/GitIcon";
+import JavaScriptIcon from "./icons/JavaScriptIcon";
+import MongoIcon from "./icons/MongoIcon";
+import NextJSIcon from "./icons/NextJSIcon";
+import NodeIcon from "./icons/NodeIcon";
+import PostgresIcon from "./icons/PostgresIcon";
+import ReactIcon from "./icons/ReactIcon";
+import TailwindIcon from "./icons/TailwindIcon";
+import TypeScriptIcon from "./icons/TypeScriptIcon";
+import ProjectCard from "./ProjectCard";
+
+const PROJECTS: Project[] = [
+  {
+    name: "Filmster",
+    summ: "Este proyecto es una página web construida con Next.js que muestra un catálogo de películas de la api TheMovieDB . Permite a los usuarios explorar diferentes películas, ver detalles y buscar títulos específicos.",
+    icons: [NextJSIcon({ width: 50, height: 50 }), JavaScriptIcon({ width: 50, height: 50 }), ReactIcon({ width: 50, height: 50 }), TailwindIcon({ width: 50, height: 50 }),GitIcon({ width: 50, height: 50 }),GithubIcon({ width: 50, height: 50 })],
+    url: "https://github.com/oasrcode/Filmster",
+  },
+  {
+    name: "iot-Simulator",
+    summ: "Proyecto que simula la generación de datos de sensores de aire y agua, para ser almacenados en mongo y de series temporales en cratedb.",
+    icons: [NodeIcon({ width: 50, height: 50 }), DockerIcon({ width: 50, height: 50 }), PostgresIcon({ width: 50, height: 50 }), MongoIcon({ width: 50, height: 50 }),GitIcon({ width: 50, height: 50 }),GithubIcon({ width: 50, height: 50 })],
+    url: "https://github.com/oasrcode/iot-Simulator",
+  },
+  {
+    name: "Portfolio",
+    summ: "Proyecto de mi portfolio personal.",
+    icons: [NextJSIcon({ width: 50, height: 50 }),ReactIcon({ width: 50, height: 50 }), TypeScriptIcon({ width: 50, height: 50 }), TailwindIcon({ width: 50, height: 50 }), GitIcon({ width: 50, height: 50 }),GithubIcon({ width: 50, height: 50 })],
+    url: "https://github.com/oasrcode/oasrcode-web",
+  }
+];
 
 export default function Projects() {
-  return <div className="relative flex flex-col gap-10 ml-2 lg:ml-0   border-black  mt-10 lg:mt-20 "></div>;
+  return (
+    <div className="relative flex flex-col lg:flex-row lg:flex-wrap gap-7 w-full min-h-[100dvh] pt-0 lg:pt-40 mb-50 items-start justify-start lg:justify-center">
+      {PROJECTS.map((e, index) => (
+        <ProjectCard key={index} data={e}></ProjectCard>
+      ))}
+    </div>
+  );
 }
