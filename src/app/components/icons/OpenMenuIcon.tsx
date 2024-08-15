0fc3d902c104cf6f92be8556845cd3ce2dda9a33
oasrcode <oasrcode@gmail.com>
2024-08-15 11:42:29 +0000

    added new icons

---
+++ src/app/components/icons/OpenMenuIcon.tsx
@@ -0,0 +1,9 @@
+import { IconProps } from "@/app/interface/IconProps";
+
+export default function OpenMenuIcon({ width, height }: IconProps) {
+  return (
+    <svg  viewBox="0 0 50 50" width={width} height={height} fill="white">
+    <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z" />
+  </svg>
+  );
+}
