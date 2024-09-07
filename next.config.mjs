f622057d471082ec9309d9b1b181cd4443d87433
oasrcode <oasrcode@gmail.com>
2024-09-07 09:22:44 +0000

    added images remotepatterns for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:["https://aythamisantana.pages.dev"]
+    }
+};
 export default nextConfig;
